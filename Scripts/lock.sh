462b26b97bee4ae0de60e27349eb0de0e7c5b620
ENEmy <nosde@protonmail.com>
2021-01-25 20:34:20 +0700

    Add Script folder that is contains nessesary script.

---
+++ Scripts/lock.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+B='#00000000'  # blank
+C='#ffffff22'  # clear ish
+D='#00bbb8'  # verifying
+T='#ffffff'  # text
+W='#ff1c49'  # wrong
+V='#00bb61'  # verifying
+
+/usr/bin/i3lock \
+--insidevercolor=$C   \
+--ringvercolor=$V     \
+\
+--insidewrongcolor=$C \
+--ringwrongcolor=$W   \
+\
+--insidecolor=$B      \
+--ringcolor=$D        \
+--linecolor=$B        \
+--separatorcolor=$D   \
+\
+--verifcolor=$T        \
+--wrongcolor=$T        \
+--timecolor=$T        \
+--datecolor=$T        \
+--layoutcolor=$T      \
+--keyhlcolor=$W       \
+--bshlcolor=$W        \
+\
+--screen 1            \
+--blur 5              \
+--clock               \
+--indicator           \
+--timestr="%H:%M:%S"  \
+--datestr="%A, %m %Y" \
+--keylayout 1         \
+--veriftext="Verifying"   \
+--wrongtext="Incorrect"   \
+--layout-font=Quark   \
+--verif-font=Quark   \
+--wrong-font=Quark   \
+--greeter-font=Quark   \
+--date-font=Quark   \
+--time-font=Quark   \
+
+# --veriftext="Drinking verification can..."
+# --wrongtext="Nope!"
+# --textsize=20
+# --modsize=10
+# --timefont=comic-sans
+# --datefont=monofur
+# etc
